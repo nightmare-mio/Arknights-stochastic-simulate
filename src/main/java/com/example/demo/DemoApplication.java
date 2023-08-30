dc16e088a81b269a4f2fc3d23a130b2518b44e26
nightmare-mio <wanglongwei2009@qq.com>
2023-08-30 17:46:01 +0800

    初始化

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,86 @@
+/*
+ * @Author: nightmare-mio wanglongwei2009@qq.com
+ * @Date: 2023-08-26 20:36:07
+ * @LastEditTime: 2023-08-29 20:51:18
+ * @Description: 
+ */
+/*
+ * @Author: nightmare-mio wanglongwei2009@qq.com
+ * @Date: 2023-08-26 20:36:07
+ * @LastEditTime: 2023-08-27 02:30:53
+ * @Description: 
+ */
+package com.example.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.service.impl.CapableServiceImpl;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	@Autowired
+	private CapableServiceImpl cap;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+
+	}
+
+	
+
+	// @Bean
+	// public ApplicationRunner executeOnStartup() {
+	// 	return args -> {
+			
+	// 		try {
+	// 			FileInputStream fileInputStream = new FileInputStream("src/main/resources/latlon.txt");
+	// 			InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream,StandardCharsets.UTF_8);
+	// 			BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+	// 			String line;
+	// 			while ((line = bufferedReader.readLine()) != null) {
+	// 				String[] temps = line.split("\\|");
+	// 				cap.save(new Capable().setName(temps[0]).setProfession(temps[1]).setRarity(temps[2]).setId(temps[5]));
+	// 			}
+
+	// 			bufferedReader.close();
+	// 			inputStreamReader.close();
+	// 			fileInputStream.close();
+	// 		} catch (IOException e) {
+	// 			e.printStackTrace();
+	// 		}
+	// 	};
+	// }
+
+	// @Bean
+	// public ApplicationRunner executeOnStartup() {
+	// 	return args -> {
+			
+	// 		try {
+	// 			FileInputStream fileInputStream = new FileInputStream("src/main/resources/latlon.txt");
+	// 			InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream,StandardCharsets.UTF_8);
+	// 			BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+	// 			String line;
+	// 			while ((line = bufferedReader.readLine()) != null) {
+	// 				String[] temps = line.split("\\|");
+	// 				cap.save(new Capable().setId(temps[0]).setName(temps[1]));
+	// 			}
+
+	// 			bufferedReader.close();
+	// 			inputStreamReader.close();
+	// 			fileInputStream.close();
+	// 		} catch (IOException e) {
+	// 			e.printStackTrace();
+	// 		}
+	// 	};
+	// }
+
+}
